759b38375735541710780973af59b9e484afdd69
Sebas Echazú <sebastianechazu@outlook.com>
2025-12-29 14:10:26 -0300

    fix: corregir la URL de la API en el entorno de producción

--- src/app/core/config/environment.prod.ts
+++ src/app/core/config/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cursala.com.ar/api/v1' ,
+  apiUrl: 'https://app.cursala.com.ar/api/v1' ,
   publicFrontend: 'https://cursala.com.ar'
 };
